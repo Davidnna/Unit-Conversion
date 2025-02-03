e96d527fb7a7a196b5bcfefc142794f3c8628a55
Davidnna <davedozie1@gmail.com>
2025-02-03 21:51:51 +0100

    correct

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const number = document.getElementById("number");
 const length = document.getElementById("length");
 const volume = document.getElementById("volume");
 const mass = document.getElementById("mass");
-const feet = 3.281;
-const gallons = 0.264;
-const pounds = 2.204;
+const copied = document.getElementById("copied");
+const error = document.getElementById("error");
+const foot = 3.281;
+const gallon = 0.264;
+const pound = 2.204;
 
 number.addEventListener("input", function () {
     number.style.width = (number.value.length + 2.5) + "ch";
@@ -16,8 +18,29 @@ number.addEventListener("input", function () {
 });
 
 function convert() {
-    const unit = number.value;
-    length.textContent = `${unit} meters = ${(unit * feet).toFixed(3)} feet | ${unit} feet = ${(unit / feet).toFixed(3)} meters`;
-    volume.textContent = `${unit} liters = ${(unit * gallons).toFixed(3)} gallons | ${unit} gallons = ${(unit / gallons).toFixed(3)} liters`;
-    mass.textContent = `${unit} kilos = ${(unit * pounds).toFixed(3)} pounds | ${unit} pounds = ${(unit / pounds).toFixed(3)} kilos`;
+    if (number.value === "" || number.value === "00") {
+        error.style.display = "block";
     }
+    else {
+        error.style.display = "none";
+        const unit = number.value;
+        const feet = (unit * foot).toFixed(3);
+        const gallons = (unit * gallon).toFixed(3);
+        const pounds = (unit * pound).toFixed(3);
+        const meters = (unit / foot).toFixed(3);
+        const liters = (unit / gallon).toFixed(3);
+        const kilos = (unit / pound).toFixed(3);
+        length.innerHTML = `<span onclick="copyText(${unit})">${unit}</span> meters = <span onclick="copyText(${feet})">${feet}</span> feet | <span onclick="copyText(${unit})">${unit}</span> feet = <span onclick="copyText(${meters})">${meters}</span> meters`;
+        volume.innerHTML = `<span onclick="copyText(${unit})">${unit}</span> liters = <span onclick="copyText(${gallons})">${gallons}</span> gallons | <span onclick="copyText(${unit})">${unit}</span> gallons = <span onclick="copyText(${liters})">${liters}</span> liters`;
+        mass.innerHTML = `<span onclick="copyText(${unit})">${unit}</span> kilos = <span onclick="copyText(${pounds})">${pounds}</span> pounds | <span onclick="copyText(${unit})">${unit}</span> pounds = <span onclick="copyText(${kilos})">${kilos}</span> kilos`;
+    }
+}
+
+function copyText(char) {
+    navigator.clipboard.writeText(char).then(() => {
+        copied.style.display = "block";
+        setTimeout(() => {
+            copied.style.display = "none";
+        }, 2000);      
+    });
+}
